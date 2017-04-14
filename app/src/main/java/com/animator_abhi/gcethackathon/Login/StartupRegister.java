f966c5eb5c83cdab375e30a4cbcef59b8fc5df05
abhinavnimesh <abhinav.nimesh9@gmail.com>
2017-04-15 00:46:32 +0530

    reg student and company working

--- app/src/main/java/com/animator_abhi/gcethackathon/Login/StartupRegister.java
+++ app/src/main/java/com/animator_abhi/gcethackathon/Login/StartupRegister.java
@@ -1,23 +1,27 @@
 package com.animator_abhi.gcethackathon.Login;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.animator_abhi.gcethackathon.Models.StartupModel;
 import com.animator_abhi.gcethackathon.R;
-import com.animator_abhi.gcethackathon.Startup.StartupMain;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class StartupRegister extends AppCompatActivity {
     Button startup_register;
+    FirebaseDatabase database;
     EditText startup_name,startup_founder,startup_description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup_register);
+        database = FirebaseDatabase.getInstance();
+        final DatabaseReference ref = database.getReference();
         startup_name = (EditText) findViewById(R.id.startup_name);
         startup_founder = (EditText) findViewById(R.id.startup_description);
         startup_description = (EditText) findViewById(R.id.startup_founder);
@@ -25,11 +29,10 @@ public class StartupRegister extends AppCompatActivity {
         startup_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(StartupRegister.this,StartupMain.class);
-                i.putExtra("s_n",startup_name.getText().toString());
-                i.putExtra("s_f",startup_founder.getText().toString());
-                i.putExtra("s_d",startup_description.getText().toString());
-                startActivity(i);
+
+                StartupModel sm = new StartupModel(startup_name.getText().toString(), startup_founder.getText().toString()
+                        , startup_description.getText().toString());
+                ref.child("startup").child(Prefs.getUserId(StartupRegister.this)).setValue(sm);
             }
         });
 
