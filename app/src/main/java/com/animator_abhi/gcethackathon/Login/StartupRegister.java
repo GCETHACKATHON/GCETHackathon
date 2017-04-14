1896f37dd073a46e58fb7ff59ace9d734544b245
Abhimanyu Sharma <abhimanyusharma96.edu@gmail.com>
2017-04-14 16:11:09 +0530

    Branch Abhimanyu

--- app/src/main/java/com/animator_abhi/gcethackathon/Login/StartupRegister.java
+++ app/src/main/java/com/animator_abhi/gcethackathon/Login/StartupRegister.java
@@ -10,6 +10,6 @@ public class StartupRegister extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_startup_login);
+        setContentView(R.layout.activity_startup_register);
     }
 }
